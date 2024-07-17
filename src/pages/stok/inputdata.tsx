f9dc3aa7a6f5e697a64c36f72b529b8700587331
Ianroery <ianroeri@gmail.com>
2024-07-18 04:24:06 +0700

    Tahap routing bagian Home, keranjang, history order, pelanggan dan  product

--- src/pages/stok/inputdata.tsx
+++ src/pages/stok/inputdata.tsx
@@ -1,98 +1,106 @@
 import Layout from "@/components/Layout";
 import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
 
 export default function Inputdatastok() {
   return (
     <>
       <Layout>
         <Navbar />
-        <div className="lg:px-10 md:px-6 sm:px-5 min-h-screen flex items-center justify-center">
-          <div className="bg-slate-500 w-[500px] rounded-lg p-6">
-            <div className="flex justify-center">
-              <h3 className="text-lg font-semibold text-gray-900 mb-4">Formulir Tambah Sabun Baru</h3>
+        <div className="container mx-auto px-4 lg:px-10 md:px-6 sm:px-5 pt-10">
+          <div>
+            <a href=" /product">
+              <Button className="bg-[#3699FF] px-8 font-Poppine hover:bg-[#3985d2] active:bg-[#1b436b]">Return</Button>
+            </a>
+          </div>
+          <div className="lg:px-10 md:px-6 sm:px-5 min-h-screen flex items-center justify-center">
+            <div className="bg-slate-50 w-[500px] rounded-lg p-6">
+              <div className="flex justify-center">
+                <h3 className="text-lg font-semibold text-gray-900 mb-4">Formulir Tambah Sabun Baru</h3>
+              </div>
+              <form className="space-y-6">
+                <div className="mb-5">
+                  <label htmlFor="namaproduct" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Nama Product
+                  </label>
+                  <input
+                    type="text"
+                    id="namaproduct"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="nama product"
+                    required
+                  />
+                </div>
+                <div className="mb-5">
+                  <label htmlFor="stockproduct" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Stok Product
+                  </label>
+                  <input
+                    type="number"
+                    id="stokproduct"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    required
+                  />
+                </div>
+                <div className="mb-5">
+                  <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    city
+                  </label>
+                  <input
+                    type="city"
+                    id="city"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="city"
+                    required
+                  />
+                </div>
+                <div className="mb-5">
+                  <label htmlFor="uploadFoto" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Upload Foto
+                  </label>
+                  <input
+                    type="file"
+                    id="uploadFoto"
+                    accept="image/*" // Hanya menerima file gambar
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+
+                <div className="mb-5">
+                  <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    price
+                  </label>
+                  <input
+                    type="price"
+                    id="price"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="price"
+                    required
+                  />
+                </div>
+                <div className="mb-5">
+                  <label htmlFor="descripsi" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Descripsi
+                  </label>
+                  <textarea
+                    id="descripsi"
+                    rows="4" // Atur jumlah baris default, bisa diubah sesuai kebutuhan
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Masukkan descripsi..."
+                    required
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Submit
+                </button>
+              </form>
             </div>
-            <form className="space-y-6">
-              <div className="mb-5">
-                <label htmlFor="namaproduct" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Nama Product
-                </label>
-                <input
-                  type="text"
-                  id="namaproduct"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="nama product"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <label htmlFor="stockproduct" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Stok Product
-                </label>
-                <input
-                  type="number"
-                  id="stokproduct"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder=""
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <label htmlFor="city" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  city
-                </label>
-                <input
-                  type="city"
-                  id="city"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="city"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <label htmlFor="uploadFoto" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Upload Foto
-                </label>
-                <input
-                  type="file"
-                  id="uploadFoto"
-                  accept="image/*" // Hanya menerima file gambar
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
-                />
-              </div>
-
-              <div className="mb-5">
-                <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  price
-                </label>
-                <input
-                  type="price"
-                  id="price"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="price"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <label htmlFor="descripsi" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Descripsi
-                </label>
-                <textarea
-                  id="descripsi"
-                  rows="4" // Atur jumlah baris default, bisa diubah sesuai kebutuhan
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Masukkan descripsi..."
-                  required
-                />
-              </div>
-
-              <button
-                type="submit"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Submit
-              </button>
-            </form>
           </div>
         </div>
       </Layout>
--- src/pages/stok/tabelstok.tsx
+++ src/pages/stok/tabelstok.tsx
@@ -2,7 +2,7 @@ import { Comboboxstok } from "@/components/combobox";
 import Header from "../home/header";
 import Layout from "@/components/Layout";
 import Navbar from "@/components/navbar";
-import { PaginationHistory } from "@/components/paginat";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -11,17 +11,21 @@ export default function Stok() {
     <>
       <Layout>
         <Navbar />
-        <div className="lg:px-10 md:px-6 sm:px-5 ">
+        <div className="lg:px-10 md:px-6 sm:px-5 mb-10">
           <Header />
           <div className="grid justify-center">
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-[900px] rounded-full ">
-              <div className="p-9 bg-slate-100 ">
-                <div className="flex justify-end gap-3">
-                  <Button className="bg-[#3699FF] text-white px-9">Sabun Baru</Button>
-                  <Button className="bg-[#4EF65F] text-white">Tambah Amount</Button>
-                </div>
-                <div className="pt-8 flex justify-between">
-                  <h2 className="font-Roboto text-[20px] font-semibold">Semua data product</h2>
+            <div className="relative overflow-x-auto w-[900px]">
+              <div className="flex justify-end gap-3 pb-2">
+                <a href="/formproduct">
+                  <Button className="bg-[#3699FF] text-white px-9 hover:bg-[#22578d] active:bg-[#1d4369]">Sabun Baru</Button>
+                </a>
+                <a href="/formproducttambah">
+                  <Button className="bg-[#4EF65F] text-white hover:bg-[#23832d] active:bg-[#1b6222]">Tambah Amount</Button>
+                </a>
+              </div>
+              <div className="p-9 bg-slate-50 rounded-xl =shadow-lg shadow-slate-300">
+                <div className="pt-2 flex justify-between items-center py-4">
+                  <h2 className="font-Poppine text-[20px] font-semibold">Semua data product</h2>
                   <div className="flex gap-2">
                     <div>
                       <Input type="search" placeholder="search" />
@@ -32,30 +36,30 @@ export default function Stok() {
                   </div>
                 </div>
                 <div className="bg-slate-400 rounded-lg">
-                  <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400  ">
-                    <thead className="text-xs text-gray-700 uppercase  dark:bg-gray-900 dark:text-gray-800 rounded-full">
-                      <tr className="text-gray-500">
-                        <th scope="col" className="px-6 py-3">
+                  <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase dark:bg-gray-900 dark:text-gray-800 rounded-full">
+                      <tr className="text-gray-200">
+                        <th scope="col" className="px-6 py-3 text-center">
                           Nama Product
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3 text-center">
                           Stok
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3 text-center">
                           Descripsi
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3 text-center">
                           Price
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3 text-center">
                           Image Profile
                         </th>
-                        <th scope="col" className="px-6 py-3">
-                          aksi
+                        <th scope="col" className="px-6 py-3 text-center">
+                          Aksi
                         </th>
                       </tr>
                     </thead>
-                    <tbody className="text-black">
+                    <tbody className="text-black font-Poppine">
                       <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                         <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">sabunx</td>
                         <td className="px-6 py-4">7</td>
@@ -98,8 +102,33 @@ export default function Stok() {
                     </tbody>
                   </table>
                 </div>
-                <div className="flex justify-start mt-4">
-                  <PaginationHistory />
+                <div className="flex justify-between flex-row-reverse">
+                  <div className="pt-2">
+                    <Pagination>
+                      <PaginationContent>
+                        <PaginationItem>
+                          <PaginationPrevious href="#" />
+                        </PaginationItem>
+                        <PaginationItem>
+                          <PaginationLink href="#">1</PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                          <PaginationLink href="#" isActive>
+                            2
+                          </PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                          <PaginationLink href="#">3</PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                          <PaginationEllipsis />
+                        </PaginationItem>
+                        <PaginationItem>
+                          <PaginationNext href="#" />
+                        </PaginationItem>
+                      </PaginationContent>
+                    </Pagination>
+                  </div>
                 </div>
               </div>
             </div>
